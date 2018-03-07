60e710e5f882f74c878feb82bc0bd369a1dfaafc
Kleyson Rios <kleysonr@yahoo.com.br>
2018-03-07 06:39:30 -0300

    - initial commit spring-websocket

---
+++ src/main/java/com/krios/pentaho/plugin/classic/hzwebsocket/services/SocketHandler.java
@@ -0,0 +1,34 @@
+package com.krios.pentaho.plugin.classic.hzwebsocket.services;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.google.gson.Gson;
+
+public class SocketHandler extends TextWebSocketHandler {
+	
+	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws InterruptedException, IOException {
+		
+		for(WebSocketSession webSocketSession : sessions) {
+			Map value = new Gson().fromJson(message.getPayload(), Map.class);
+			webSocketSession.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		}
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		//the messages will be broadcasted to all users.
+		sessions.add(session);
+	}	
+
+}
